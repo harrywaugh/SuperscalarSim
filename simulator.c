88c186f5b5957c4b9a110c19873f2af85bc27ae4
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-10-15 15:49:25 +0100

    Cleanup

--- simulator.c
+++ simulator.c
@@ -4,8 +4,6 @@
 #define MAX_PROGRAM_LINE_LEN 100
 
 int main(int argn, char *argv[])  {
-    FILE *fp;
-    char str[MAX_PROGRAM_LINE_LEN];
     char *filename = argv[1];
     char **program_str;
     int program_len;
@@ -16,8 +14,6 @@ int main(int argn, char *argv[])  {
     }
     printf("Program Name: %s\n", filename);
     program_len = read_program_file(filename, &program_str);
-
-
 
     return 0;
 }
